cc0cc091d7056fd35f69eeedb0b87736cb4e49ed
KjellRuiter <kjellruiter@hotmail.com>
2020-02-12 22:12:10 +0100

    verschillende routes aanmaken

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('feature-dating-app'));
+app.use(express.static('realface'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get ("/contact", (req, res) => {
+    res.send ('Kjell Ruiter');
 });
 
 app.use(function(req, res){
