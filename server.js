8701a76a9380f9c5f436705eef675f4ad0a2eabb
KjellRuiter <kjellruiter@hotmail.com>
2020-02-27 14:12:39 +0100

    Ik ben bezig geweest met include waarin ik head en css inlaad.

--- server.js
+++ server.js
@@ -5,29 +5,30 @@ const path = require('path');
 
 app.use(express.static('static'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/static/index.html'));
-});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get ("/contact", (req, res) => {
-    res.send ('Kjell Ruiter tel: 0623555040');
-});
+app.get("/", (req, res) => res.render("pages/index"));
 
-app.get ("/about", (req, res) => {
-    res.send ('Dit is realface de dating app');
-});
+// app.get ("/contact", (req, res) => {
+//     res.send ('Kjell Ruiter tel: 0623555040');
+// });
 
-app.get('/create', function(req, res) {
-    res.sendFile(path.join(__dirname+'/static/create.html '));
-});
+// app.get ("/about", (req, res) => {
+//     res.send ('Dit is realface de dating app');
+// });
 
-app.get('/landing/name/:nameId/gender/:genderId', function (req, res) {
-    res.send(req.params)
-  });
+// app.get('/create', function(req, res) {
+//     res.sendFile(path.join(__dirname+'/static/create.html '));
+// });
 
-app.use(function(req, res){
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 Not Found')
-})
+// app.get('/landing/name/:nameId/gender/:genderId', function (req, res) {
+//     res.send(req.params)
+//   });
+
+// app.use(function(req, res){
+//     res.type('text/plain')
+//     res.status(404)
+//     res.send('404 Not Found')
+// })
 app.listen(3000, () => console.log('App listening on port 3000!')); 
