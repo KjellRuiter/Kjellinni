bf1303d07a8ce6f017f13750ea8250aad3923e4f
KjellRuiter <kjellruiter@hotmail.com>
2020-03-15 22:49:23 +0100

    script opgesplitst zodat ik geen console error meer krijg

---
+++ static/login_section.js
@@ -0,0 +1,9 @@
+// hide section home
+const bigSection = document.querySelector('.welcom_screen');
+function removeOverlay() {
+  setTimeout(function() {
+    bigSection.classList.add('hide_item_timout');
+  }, 500);
+}
+
+removeOverlay();
--- static/script.js
+++ static/script.js
@@ -7,13 +7,3 @@ function myFunction() {
     x.type = 'password';
   }
 }
-
-// hide section home
-const bigSection = document.querySelector('.welcom_screen');
-function removeOverlay() {
-  setTimeout(function() {
-    bigSection.classList.add('hide_item_timout');
-  }, 500);
-}
-
-removeOverlay();
