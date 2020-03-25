ce38a40fece17d0bb6fb82f9eb47c19f7db59a60
LaupWing <loc_strict@hotmail.com>
2020-03-25 21:35:50 +0100

    added: Dummy data

--- matching/getMatch.js
+++ matching/getMatch.js
@@ -1,9 +1,9 @@
 const User = require('../users/user.model');
 
-module.exports = (user)=>{
-    const allPossibleMatches = User
+module.exports = async (user)=>{
+    const allPossibleMatches = await User
         .find({})
         .where('gender', user.gender === 'male' ? 'female' : 'male')
     
     const fitleredOut = allPossibleMatches.filter()
-}
+} 
