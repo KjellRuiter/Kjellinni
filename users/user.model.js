5fdeeabdfa2c1730e0a4eb7c42753c3f6f5df646
LaupWing <loc_strict@hotmail.com>
2020-03-26 13:48:06 +0100

    added prettier file

--- users/user.model.js
+++ users/user.model.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
-  email: { type: String, unique: true, required: true },
-  hash: { type: String, required: true },
-  name: { type: String, required: true },
-  gender: { type: String, required: false },
-  photo: { type: String, required: false },
-  age: { type: String, required: false },
-  hobby: { type: String, required: false },
-  intrested: { type: String, required: false },
-  createdDate: { type: Date, default: Date.now },
+    email: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    name: { type: String, required: true },
+    gender: { type: String, required: false },
+    photo: { type: String, required: false },
+    age: { type: String, required: false },
+    hobby: { type: String, required: false },
+    intrested: { type: String, required: false },
+    createdDate: { type: Date, default: Date.now },
 });
 
 // This is not stored in the database its only for mongoose to make a relationship
-schema.virtual('matches',{
+schema.virtual('matches', {
     ref: 'Matches',
     localField: '_id',
-    foreignField: 'owner'
-})
+    foreignField: 'owner',
+});
 
 // schema.set('toJSON', { virtuals: true });
 
