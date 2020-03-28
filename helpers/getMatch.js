0b61d6d9c66181bdac988279e1306cb43f7e7ec8
LaupWing <loc_strict@hotmail.com>
2020-03-28 12:14:31 +0100

    can find match

--- helpers/getMatch.js
+++ helpers/getMatch.js
@@ -1,13 +1,34 @@
 const User = require('../database/models/user')
+const Matches = require('../database/models/matches')
+const randomItem = require('random-item')
 
 module.exports = async (user, matches) => {
+  if (matches.currentlyMatching) {
+    const user = await User.findById(matches.currentlyMatching)
+    return user
+  }
   const allPossibleMatches =
     user.gender === 'both'
       ? await User.find({})
       : await User.find({}).where(
           'gender',
-          user.gender === 'male' ? 'female' : 'male',
+          user.gender === 'Man' ? 'Vrouw' : 'Man',
         )
-  console.log(user)
-  const fitleredOut = allPossibleMatches.filter()
+  const filtered = allPossibleMatches.filter(u => {
+    const alreadyMatched = matches.matched_history.find(u2 =>
+      u2._id.equals(u._id),
+    )
+    if (!alreadyMatched) {
+      return u
+    }
+  })
+  const random = randomItem(filtered)
+  try {
+    await Matches.findByIdAndUpdate(matches._id, {
+      currentlyMatching: random._id,
+    })
+  } catch (e) {
+    console.log(e)
+  }
+  return random
 }
--- routes/match/match.js
+++ routes/match/match.js
@@ -1,5 +1,8 @@
+const getMatch = require('../../helpers/getMatch')
 module.exports = class {
-  static getMethod(req, res) {
+  static async getMethod(req, res) {
+    const match = await getMatch(req.session.user, req.session.matches)
+    console.log(match)
     res.render('pages/match')
   }
 }
