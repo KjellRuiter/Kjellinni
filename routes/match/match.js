81d06df7d2d0e375cf85ff35debaf898775adf0c
remcopasma <rempiepasma@gmail.com>
2020-03-29 13:14:03 +0200

    started working on display a match

--- routes/match/match.js
+++ routes/match/match.js
@@ -2,7 +2,7 @@ const getMatch = require('../../helpers/getMatch')
 module.exports = class {
   static async getMethod(req, res) {
     const match = await getMatch(req.session.user, req.session.matches)
-    console.log(match)
-    res.render('pages/match')
+    console.log('matchennnnn', match)
+    res.render('pages/match', match)
   }
 }
