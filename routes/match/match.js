3bafd3c03a808ff5969a99034e19a5422888d8c3
LaupWing <loc_strict@hotmail.com>
2020-03-29 18:21:42 +0200

    changed data type

--- routes/match/match.js
+++ routes/match/match.js
@@ -1,12 +1,24 @@
 const getMatch = require('../../helpers/getMatch')
+const User = require('../../database/models/user')
+
 module.exports = class {
     static async getMethod(req, res) {
         const match = await getMatch(req.session.user, req.session.matches)
-        console.log('matchennnnn', match)
         res.render('pages/match', { match })
     }
     static async postMethod(req, res) {
-        console.log(req.body)
+        if ('like' in req.body) {
+            const matchingUser = await User.findById(req.session.matches.currentlyMatching)
+            await matchingUser.populate('matches').execPopulate()
+            const matchingUserMatches = matchingUser.matches[0]
+
+            const checkLiked = matchingUserMatches.likes.find(id => id.equals(req.session.user._id))
+            const checkDisLiked = matchingUserMatches.dislikes.find(id => id.equals(req.session.user._id))
+
+            if (!checkDisLiked && !checkLiked) {
+
+            }
+        }
         res.redirect('/match')
     }
 }
