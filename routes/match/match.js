b0da259ebc759c701dd604ef7e3e29489fcef6e6
remcopasma <rempiepasma@gmail.com>
2020-03-29 13:27:35 +0200

    show matching profiles

--- routes/match/match.js
+++ routes/match/match.js
@@ -3,6 +3,6 @@ module.exports = class {
   static async getMethod(req, res) {
     const match = await getMatch(req.session.user, req.session.matches)
     console.log('matchennnnn', match)
-    res.render('pages/match', match)
+    res.render('pages/match', { match })
   }
 }
