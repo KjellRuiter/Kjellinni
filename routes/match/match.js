cfe7634d783953d4c93f4d8f154a42d8f0280885
LaupWing <loc_strict@hotmail.com>
2020-03-29 18:01:18 +0200

    added match post method

--- routes/match/match.js
+++ routes/match/match.js
@@ -1,8 +1,12 @@
 const getMatch = require('../../helpers/getMatch')
 module.exports = class {
-  static async getMethod(req, res) {
-    const match = await getMatch(req.session.user, req.session.matches)
-    console.log('matchennnnn', match)
-    res.render('pages/match', { match })
-  }
+    static async getMethod(req, res) {
+        const match = await getMatch(req.session.user, req.session.matches)
+        console.log('matchennnnn', match)
+        res.render('pages/match', { match })
+    }
+    static async postMethod(req, res) {
+        console.log(req.body)
+        res.redirect('/match')
+    }
 }
--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -1,7 +1,7 @@
 // Middleware
 const {
-  forwardAuthenticated,
-  ensureAuthenticated,
+    forwardAuthenticated,
+    ensureAuthenticated,
 } = require('../middleware/auth')
 const upload = require('../helpers/upload')()
 const router = new require('express').Router()
@@ -14,17 +14,18 @@ const logout = require('./auth/logout')
 const match = require('./match/match')
 
 router
-  .get('/profile', ensureAuthenticated, profile)
-  .get('/', forwardAuthenticated, login.getMethod)
-  .get('/register', forwardAuthenticated, register.getMethod)
-  .post('/users/authenticate', forwardAuthenticated, login.postMethod)
-  .post('/users/register', forwardAuthenticated, register.postMethod)
-  .put(
-    '/users/:id',
-    [ensureAuthenticated, upload.single('photo')],
-    id.putMethod,
-  )
-  .delete('/users/:id', ensureAuthenticated, id.deleteMethod)
-  .get('/users/logout', ensureAuthenticated, logout)
-  .get('/match', ensureAuthenticated, match.getMethod)
+    .get('/profile', ensureAuthenticated, profile)
+    .get('/', forwardAuthenticated, login.getMethod)
+    .get('/register', forwardAuthenticated, register.getMethod)
+    .post('/users/authenticate', forwardAuthenticated, login.postMethod)
+    .post('/users/register', forwardAuthenticated, register.postMethod)
+    .put(
+        '/users/:id',
+        [ensureAuthenticated, upload.single('photo')],
+        id.putMethod,
+    )
+    .delete('/users/:id', ensureAuthenticated, id.deleteMethod)
+    .get('/users/logout', ensureAuthenticated, logout)
+    .get('/match', ensureAuthenticated, match.getMethod)
+    .post('/match', ensureAuthenticated, match.postMethod)
 module.exports = router
