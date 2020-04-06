e90bfc4138d5788a545be6424a74dfd0531a2c04
LaupWing <loc_strict@hotmail.com>
2020-04-06 18:22:01 +0200

    models update

---
+++ database/models/songs.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const schema = new Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId, // Data store owner is objectid
+        required: true,
+        ref: 'User', // set up the relationship between task and user
+    }
+})
+
+module.exports = mongoose.model('Songs', schema)
