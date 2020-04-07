7f908b586a1a64f1011883188f663193ebfd1208
admirai <isabel.admiraal@hva.nl>
2020-04-07 14:23:10 +0200

    Deleted Spotify test environment.

--- spotify/routes/GET/login.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  res.send(`
-        <a href="/spotify/login">Login with spotify</a>
-    `)
-}
--- spotify/routes/GET/spotify.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-
-module.exports = settings =>
-  // Endpoint request parameters
-  fetch(`https://api.spotify.com/v1/${settings.endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${settings.acces_token}`,
-    },
-  }).then(response => response.json())
