9aaff3214465bac08fa19be04aa2abe882f001d1
LaupWing <loc_strict@hotmail.com>
2020-04-08 14:45:08 +0200

    added generate user

--- automation/generateUser.py
+++ automation/generateUser.py
@@ -1,9 +1,13 @@
 from datetime import datetime 
 
-now = str((datetime.now()).timestamp())
-pointIndex = now.find('.')
-secondsNow = now[0:pointIndex]
+def createDummyUser():
+    now = str((datetime.now()).timestamp())
+    pointIndex = now.find('.')
+    secondsNow = now[0:pointIndex]
 
-automatedUser = f'testUser{secondsNow}'
-
-print(automatedUser)
+    automatedUser = f'testUser{secondsNow}'
+    return {
+        "name": automatedUser,
+        "email": f'{automatedUser}@hotmail.com',
+        "password": "testtest123"
+    }
--- automation/make_account.py
+++ automation/make_account.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from url import url
+from generateUser import createDummyUser
 import os 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -8,7 +9,8 @@ chrome_browser = webdriver.Chrome(f'{dir_path}/chromedriver')
 chrome_browser.maximize_window()
 chrome_browser.get(url)
 
+register_btn = chrome_browser.find_element_by_css_selector('.message a')
+register_btn.click()
 
-
-chrome_browser.close()
-chrome_browser.close()
+# chrome_browser.close()
+# chrome_browser.close()
