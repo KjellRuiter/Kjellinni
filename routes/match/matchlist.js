22f84c87f2e135876e3eda63b974e530a9ff02f3
remcopasma <rempiepasma@gmail.com>
2020-04-09 13:51:19 +0200

    pass user accepted collection

--- routes/match/matchlist.js
+++ routes/match/matchlist.js
@@ -1,5 +1,7 @@
-module.exports = (req, res)=>{
-    res.render('pages/matchlist', { match })
-   }
-
-const getAcceptedMatches = (list)=> list.filter(user => user.status==='accepted')
+module.exports = class {
+    static async getMethod(req, res) {
+        const matches = await getMatches(process.env.DB_MATCHES);
+        const getAcceptedMatches = (list)=> list.filter(user => user.status==='accepted')
+        res.render('pages/matches', {user: req.session.matches.matched_history })
+    }
+}
