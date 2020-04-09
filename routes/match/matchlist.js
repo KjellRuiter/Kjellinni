9acc4938edd23ece1998d66ca07daaf628029549
LaupWing <loc_strict@hotmail.com>
2020-04-09 16:20:18 +0200

    Allusers are the users you can use

--- routes/match/matchlist.js
+++ routes/match/matchlist.js
@@ -1,8 +1,12 @@
+const User = require('../../database/models/user')
+
 module.exports = class {
     static async getMethod(req, res) {
-        console.log(req.session.matches)
-        res.send('test')
         const getAcceptedMatches = req.session.matches.matched_history.filter(user => user.status==='accepted')
+        
+        const userPromises = getAcceptedMatches.map(user=> User.findById(user.userId))
+        const allUsers = await Promise.all(userPromises)
+        
         res.render('pages/matches', {user:getAcceptedMatches  })
     }
 }
