c7b5a50c01df636590c5da1ef0eef24b1bc976db
remcopasma <rempiepasma@gmail.com>
2020-04-10 11:58:08 +0200

    remove white space

--- routes/match/matchlist.js
+++ routes/match/matchlist.js
@@ -7,6 +7,6 @@ module.exports = class {
         const userPromises = getAcceptedMatches.map(user=> User.findById(user.userId))
         const allUsers = await Promise.all(userPromises)
         console.log(allUsers)
-        res.render('pages/matchlist', {user:getAcceptedMatches  })
+        res.render('pages/matchlist', {user:getAcceptedMatches})
     }
 }
