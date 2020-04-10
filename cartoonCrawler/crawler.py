07e66d85b0ba815f340839ab57dc047a5b6a5d7c
LaupWing <loc_strict@hotmail.com>
2020-04-10 02:54:30 +0200

    finsihing up

--- cartoonCrawler/crawler.py
+++ cartoonCrawler/crawler.py
@@ -1,4 +1,5 @@
 import requests 
+from random import randint
 from bs4 import BeautifulSoup
 
 
@@ -13,7 +14,7 @@ def getLinks(link):
         links.append(href)
     return links
 
-def getDetailsCharacter(endpoint):
+def getDetailsCharacter(endpoint, gender):
     baseUrl = 'https://en.wikipedia.org/'
     res = requests.get(f'{baseUrl}{endpoint}')
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -21,32 +22,32 @@ def getDetailsCharacter(endpoint):
     info = soup.find('table', {"class": "infobox"}) if soup.find('table', {"class": "infobox"}) else soup.find('div', {"class": "thumbinner"})
     name =  soup.find(id='firstHeading').getText()
     img = info.find('img')['src'] if (info and info.find('img')) else None
-    print(name)
-    # print(info)
-    if not img:
+
+    if not img or 'List' in name or 'and ' in name:
         return None
     return{
-        "name": name.strip(),
+        "name": name.strip().replace('\'', ' '),
         "img": img,
-        "endpoint": endpoint
+        "endpoint": endpoint,
+        "gender": gender,
+        "age": str(randint(18,80))
     }
 
-def cleanup(character):
-    print(character)
-    # return{
-    #     "name": character['name'].rstrip(),
-    #     "img": character['img']
-    # }
 
 
 linksMale = getLinks('https://en.wikipedia.org/wiki/Category:Male_characters_in_animation')
 linksFemale  = getLinks('https://en.wikipedia.org/wiki/Category:Female_characters_in_animation')
 
-test = list(filter(lambda x: x, [getDetailsCharacter(link) for link in linksMale]))
-# test2 = [cleanup(char) for char in test]
-# for char in test:
-#     print(char.name)
-print(test)
+detailMales = list(filter(lambda x: x, [getDetailsCharacter(link, 'Man') for link in linksMale]))
+detailFemales = list(filter(lambda x: x, [getDetailsCharacter(link, 'Female') for link in linksFemale]))
+
+with open("males.json", "w") as json_file:
+    stringed = str(detailMales).replace('\'', '"')
+    json_file.write(stringed)
+with open("females.json", "w") as json_file:
+    stringed = str(detailFemales).replace('\'', '"')
+    json_file.write(stringed)
+
 
 
 
