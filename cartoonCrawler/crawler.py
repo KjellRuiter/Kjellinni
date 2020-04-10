980b95d2c6f256f1120576cb6e954550aa459f6e
LaupWing <loc_strict@hotmail.com>
2020-04-10 02:21:33 +0200

    maybe right and remove now

--- cartoonCrawler/crawler.py
+++ cartoonCrawler/crawler.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
 def getLinks(link):
     res = requests.get(link)
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -14,23 +13,41 @@ def getLinks(link):
         links.append(href)
     return links
 
-def getDetailsCharacter(endpoint, gender):
+def getDetailsCharacter(endpoint):
     baseUrl = 'https://en.wikipedia.org/'
     res = requests.get(f'{baseUrl}{endpoint}')
     soup = BeautifulSoup(res.text, 'html.parser')
 
-    info = soup.find('table', {"class": "infobox"})
-    name = info.find('th').getText()
-    img = info.find('img')['src']
-
+    info = soup.find('table', {"class": "infobox"}) if soup.find('table', {"class": "infobox"}) else soup.find('div', {"class": "thumbinner"})
+    name =  soup.find(id='firstHeading').getText()
+    img = info.find('img')['src'] if (info and info.find('img')) else None
+    print(name)
+    # print(info)
+    if not img:
+        return None
     return{
-        "name": name,
+        "name": name.strip(),
         "img": img,
-        "gender": gender
+        "endpoint": endpoint
     }
+
+def cleanup(character):
+    print(character)
+    # return{
+    #     "name": character['name'].rstrip(),
+    #     "img": character['img']
+    # }
+
 
 linksMale = getLinks('https://en.wikipedia.org/wiki/Category:Male_characters_in_animation')
 linksFemale  = getLinks('https://en.wikipedia.org/wiki/Category:Female_characters_in_animation')
+
+test = list(filter(lambda x: x, [getDetailsCharacter(link) for link in linksMale]))
+# test2 = [cleanup(char) for char in test]
+# for char in test:
+#     print(char.name)
+print(test)
+
 
 
 # print(linksFemale)
