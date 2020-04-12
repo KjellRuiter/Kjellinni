c398fd1c7924e4218136ab963368a6b3355c9fe4
Timilof <zinctim@gmail.com>
2020-04-13 00:28:59 +0200

    refactored matchlist page

--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -1,58 +1,73 @@
 const getFromDB = require('../../helpers/getFromDb')
 const setMessage = require('../../helpers/setMessage')
 const updateLastSent = require('../../helpers/updateLastSent')
-const selectUserImg = require('../../helpers/selectUserImg')
+const filterPartenerData = require('../../helpers/filterPartenerData')
 const deleteMatch = require('../../helpers/deleteMatch')
 
 module.exports = class {
   static async renderChat(req, res) {
-    const chatData = await getFromDB(req.params.match)
-    const matchData = await getFromDB(process.env.DB_MATCHES, req.params.match)
-    // make the user value come from req.session.user
-    const partnerData = selectUserImg(matchData, 'Janno')
+      // TODO: add req.session.user for sender
+      const bothID = ["ID12345678910", req.params.id]
+    let chatData = await getFromDB(process.env.DB_CHATS, bothID, true)
+    if(chatData.length < 1){
+        const dataObject = {
+      // TODO: add req.session.user for sender
+            users: [req.params.id, "ID12345678910"]
+        };
+        await setMessage(process.env.DB_CHATS, dataObject, true).then(
+            async () => {
+                chatData = await getFromDB(process.env.DB_CHATS, req.params.id, true)
+            });
+    }
+    const partnersData = await getFromDB(process.env.DB_MATCHES, req.params.id, false)
+    const filteredPartnersData = filterPartenerData(partnersData)
+    console.log("coming from chatjs route",partnersData)
     res.render('pages/chat', {
-      chatHistory: chatData,
-      roomID: req.params.match,
-      user: 'Janno',
-      partner: partnerData,
-    })
+        chatHistory: chatData[0].chat_history,
+        roomID: chatData[0]._id,
+      // TODO: add req.session.user for sender
+        user: "ID12345678910", 
+        partner: filteredPartnersData,
+      })
   }
   static async sendMessage(req, res) {
-    updateLastSent(
-      process.env.DB_MATCHES,
-      req.params.match,
-      req.body.textMessage,
-    )
+    // updateLastSent(
+    //   process.env.DB_MATCHES,
+    //   req.params.match,
+    //   req.body.textMessage,
+    // )
     const messageData = {
       // TODO: add req.session.user for sender
-      sender: 'Janno',
-      msg: req.body.textMessage,
-      time: new Date().getHours() + ':' + new Date().getMinutes(),
+      sender: 'ID12345678910',
+      message: req.body.textMessage,
+      created_at: new Date().getHours() + ':' + new Date().getMinutes(),
     }
-    await setMessage(messageData, req.params.match).then(
-      async () => {
-        const chatData = await getFromDB(req.params.match)
-        const matchData = await getFromDB(
-          process.env.DB_MATCHES,
-          req.params.match,
-        )
-        // make the user value come from req.session.user
-        const partnerData = selectUserImg(matchData, 'Janno')
-        res.render('pages/chat', {
-          chatHistory: chatData,
-          roomID: req.params.match,
-          user: 'Janno',
-          partner: partnerData,
-        })
-      },
-      err => {
-        console.error(err)
-      },
-    )
+    console.log(messageData)
+
+   let chatData = await getFromDB(process.env.DB_CHATS, req.params.id, false)
+   chatData[0].chat_history.push(messageData)
+   console.log("coming from setting message",chatData[0].chat_history)
+   await setMessage(process.env.DB_CHATS, chatData[0].chat_history, false, req.params.id)
+
+    //     async () => {
+    //     const partnersData = await getFromDB(process.env.DB_MATCHES, req.params.id, false)
+    //     const filteredPartnersData = filterPartenerData(partnersData)
+    //     // res.render('pages/matchlist', {
+    //         // chatHistory: chatData[0].chat_history,
+    //         // roomID: chatData[0]._id,
+    //       // TODO: add req.session.user for sender
+    //         // user: "ID12345678910", 
+    //         // partner: filteredPartnersData,
+    //     //   })
+    //   },
+    //   err => {
+    //     console.error(err)
+    //   },
+    
   }
   static async unMatch(req, res) {
     await deleteMatch(process.env.DB_MATCHES, req.params.room).then(
-      async value => {
+      async () => {
         const matches = await getFromDB(process.env.DB_MATCHES)
         const filteredMatches = splitMatches(matches)
         // make the user come from req.session.user
