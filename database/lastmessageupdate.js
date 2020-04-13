dd5ed2886a2e6cc2fd002c5a98e707c938fad079
LaupWing <loc_strict@hotmail.com>
2020-04-13 14:57:03 +0200

    added param

--- database/lastmessageupdate.js
+++ database/lastmessageupdate.js
@@ -1,5 +1,15 @@
 const Matches = require('./models/matches')
 
+/**
+ * Saves a file in imgur as base64 and returns the image link in astring
+ * @function lastmessageupdate
+ * @public
+ * @param {matches} - whole matches collection
+ * @param {otherId} - Other user id (the user you are sending an message)
+ * @param {senderId} - Who sended the message
+ * @param {message} - The message it self
+ */
+
 module.exports =async (matches, otherId, senderId, message)=>{
     const updateMatchedHistory = matches.matched_history.map(match=>{
         if(match.userId.equals(otherId)){
