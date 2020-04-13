1721680ea9d677c2490f87f1c0674c6b28187461
admirai <isabel.admiraal@hva.nl>
2020-04-13 10:13:21 +0200

    Removed delete function.

--- routes/user/profile.js
+++ routes/user/profile.js
@@ -34,13 +34,4 @@ module.exports = async (req, res) => {
   }
   req.session.matches = req.user.matches
   res.render('pages/profile', { user: req.user, song: songInDb })
-
-  // delete
-  // const remove = () => {
-  //   Songs.deleteOne({ _id: req.session.user.songs._id }, e => {
-  //     if (e) res.status(500).send(e)
-  //     else res.sendStatus(200)
-  //   })
-  // }
-  // remove()
 }
